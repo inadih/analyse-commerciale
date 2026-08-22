a112cc1176daa2655abbcee680daad467edcbbfe
Islam Nadih <nthgtodoeverywhere@gmail.com>
2026-08-22 16:43:59 +0200

    feat: fct_ventes en incremental (merge sur order_id) + tests + doc

--- models/marts/fct_ventes.sql
+++ models/marts/fct_ventes.sql
@@ -1,21 +1,29 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='order_id'
+    )
+}}
+
 with ventes as (
 
     select * from {{ ref('stg_ventes') }}
 
+    {% if is_incremental() %}
+    -- Ne s'active QUE lors des runs incrémentaux :
+    -- on ne prend que les ventes plus récentes que la date max déjà chargée.
+    where date_vente > (select max(date_key) from {{ this }})
+    {% endif %}
+
 )
 
 select
-    -- clé de la transaction (dimension dégénérée)
     order_id,
-
-    -- clés étrangères vers les dimensions (mêmes colonnes que dans les dims)
     {{ dbt_utils.generate_surrogate_key(['categorie', 'produit']) }} as produit_key,
     {{ dbt_utils.generate_surrogate_key(['client']) }}              as client_key,
     {{ dbt_utils.generate_surrogate_key(['pays']) }}                as geographie_key,
     {{ dbt_utils.generate_surrogate_key(['canal_vente']) }}         as canal_key,
     cast(date_vente as date)                                        as date_key,
-
-    -- mesures
     quantite,
     prix_unitaire,
     remise,
@@ -23,5 +31,4 @@ select
     montant_total,
     montant_total - remise - frais_livraison                        as marge,
     satisfaction
-
 from ventes
